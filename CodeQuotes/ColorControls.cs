ab5706049a4dd7498ebaa4cbfaf0389a3a7641f5
Wolvye <alainsoltau@googlemail.com>
2025-06-03 19:47:14 +0200

    Uff was für eine schwere Geburt

---
+++ CodeQuotes/ColorControls.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace MaterialColorUtilities
+{
+    public static class ColorControls
+    {
+        public static List<Color> GetColorGradient(Color start, Color end, int steps)
+        {
+            var gradient = new List<Color>();
+            for (int i = 0; i < steps; i++)
+            {
+                float ratio = (float)i / (steps - 1);
+                float r = start.Red + (end.Red - start.Red) * ratio;
+                float g = start.Green + (end.Green - start.Green) * ratio;
+                float b = start.Blue + (end.Blue - start.Blue) * ratio;
+                gradient.Add(new Color(r, g, b));
+            }
+            return gradient;
+        }
+
+    }
+}
--- CodeQuotes/MainPage.xaml.cs
+++ CodeQuotes/MainPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿namespace CodeQuotes
+﻿using MaterialColorUtilities;
+
+namespace CodeQuotes
 {
     public partial class MainPage : ContentPage
     {
-        
+        private readonly Random random = new();
+
         public MainPage()
         {
             InitializeComponent();
@@ -10,6 +13,40 @@
 
         private void OnCounterClicked(object? sender, EventArgs e)
         {
+        }
+
+        private void btnGenerateQoute_Clicked(object sender, EventArgs e)
+        {
+            var startColor = new Color(
+                                 (float)random.NextDouble(),
+                                 (float)random.NextDouble(),
+                                 (float)random.NextDouble());
+
+            var endColor = new Color(
+                                (float)random.NextDouble(),
+                                (float)random.NextDouble(),
+                                (float)random.NextDouble());
+
+
+            var colors = ColorControls.GetColorGradient(startColor, endColor, 6);
+
+
+
+            float stopOffset = .0f;
+            var stops = new GradientStopCollection();
+
+            foreach (var c in colors)
+            {
+                stops.Add(new GradientStop(c, stopOffset));
+                stopOffset += .2f;
+            }
+
+            var gradient =
+                new LinearGradientBrush(stops,
+                new Point(0, 0),
+                new Point(1, 1));
+
+            background.Background = gradient;
         }
     }
 }
